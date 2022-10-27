34e366030cc9b586c567ae6cd7db03e9d26a3c3f
haipng <haiphamece@gmail.com>
2022-10-27 11:16:20 +0700

    add death notification handling for client

--- helloservice/client.cpp
+++ helloservice/client.cpp
@@ -3,9 +3,24 @@
 #include <BpHelloService.h>
 #include <binder/IServiceManager.h>
 
+#include <binder/IPCThreadState.h>
+#include <binder/ProcessState.h>
+
 using namespace std;
 using namespace android;
 
+class HelloSvcDeathRecipient : public IBinder::DeathRecipient {
+    public:
+        HelloSvcDeathRecipient() {
+            cout << "client: create HelloSvcDeathRecipient\n";
+        };
+        void binderDied(const wp<IBinder>& who) {
+            cout << "!!!!!!!!\n";
+            cout << "client: HelloService has died. Terminate client.\n";
+            exit(0);
+        };
+
+};
 
 String16 serviceName("helloservice");
 
@@ -28,6 +43,20 @@ int main(int argc, char *argv[]) {
 
     cout << "client: recv from server: " << service->sayhello_to(String8("Jackson")) << endl;
 
-    cout << "client add integer: " << service->addInts(35, 36) << endl;
+    cout << "client: add integer: " << service->addInts(35, 36) << endl;
+
+    cout << endl << endl;
+
+    sp<HelloSvcDeathRecipient> dr=(new HelloSvcDeathRecipient());
+    cout << "client: linktoDeath returns " << binder->linkToDeath(dr) << endl << endl;
+
+    sp<ProcessState> ps(ProcessState::self());
+    ps->startThreadPool();
+
+    while(1) {
+        cout << "client: recv from server: " << service->sayhello() << endl;
+        
+        sleep(1);
+    }
 }
 
